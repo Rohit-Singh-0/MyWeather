0915bbd46db8431245e3b35f1335816baa7dda0a
Rohit-Singh-0 <135234916+Rohit-Singh-0@users.noreply.github.com>
2023-07-01 14:38:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const humidity = document.getElementById("humidity")
 const wind = document.getElementById("wind")
 const feels_like = document.getElementById("feels_like")
 const cloudy = document.getElementById("cloudy")
-
+const weather_img = document.getElementById("weather_png")
+const bg_img = document.querySelector(".bg-img")
+const content_bg = document.querySelector(".content-bg")
 
 
 
@@ -26,6 +28,46 @@ async function check_weather(city){
     wind.innerHTML = `${Math.round(weather_data.wind.speed)}Km/h`
     feels_like.innerHTML = `${Math.round(weather_data.main.feels_like-273.15)}&degC`
     cloudy.innerHTML = `${weather_data.clouds.all}%`
+
+
+    switch (weather_data.weather[0].main) {
+        case 'Clouds':
+            weather_img.src = "/assets/cloud.png"
+            bg_img.src = "/assets/cloud-img.jpg"
+            content_bg.src = "/assets/cloud-img.jpg"
+            break;
+        case 'Clear':
+            weather_img.src = "/assets/clear.png"
+            bg_img.src = "/assets/clear-img.jpg"
+            content_bg.src = "/assets/clear-img.jpg"
+            break;
+        case 'Mist':
+            weather_img.src = "/assets/mist.png"
+            bg_img.src = "/assets/mist-img.jpg"
+            content_bg.src = "/assets/mist-img.jpg"
+            break;
+        case 'Haze':
+            weather_img.src = "/assets/mist.png"
+            bg_img.src = "/assets/mist-img.jpg"
+            content_bg.src = "/assets/mist-img.jpg"
+            break;
+        case 'Rain':
+            weather_img.src = "/assets/rain.png"
+            bg_img.src = "/assets/rain-img.jpg"
+            content_bg.src = "/assets/rain-img.jpg"
+            break;
+        case 'Drizzle':
+            weather_img.src = "/assets/rain.png"
+            bg_img.src = "/assets/rain-img.jpg"
+            content_bg.src = "/assets/rain-img.jpg"
+            break;
+        case 'Snow':
+            weather_img.src = "/assets/snow.png"
+            bg_img.src = "/assets/snow-img.jpg"
+            content_bg.src = "/assets/snow-img.jpg"
+            break;
+        
+    }
 
 }
 
